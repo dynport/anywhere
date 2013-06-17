e2493f0c84c08b075e5bf08de361c0adcfc48a1e
Tobias Schwab <tobias.schwab@dynport.de>
2013-06-17 15:30:03 +0200

    use rainbow instead of colorizing, add support for log_levels

--- lib/anywhere/logger.rb
+++ lib/anywhere/logger.rb
@@ -1,14 +1,36 @@
-require "colorize"
+require "rainbow"
 require "time"
+require "logger"
 
 module Anywhere
   class Logger
+    LEVELS = {
+      DEBUG:    0,
+      INFO:     1,
+      WARN:     2,
+      ERROR:    3,
+      FATAL:    4,
+      UNKNOWN:  5
+    }
+
+    COLORS = {
+      0 => "#6d6d6d", # 109, 109, 109
+      1 => "#227000", # 34, 112
+      2 => "#f79700", # 247, 151
+      3 => "#f00000", # 240, 0
+    }
+
     attr_accessor :prefix
+    attr_writer :log_level
 
     class << self
       def mutex
         @mutex ||= Mutex.new
       end
+    end
+
+    def log_level
+      @log_level ||= ::Logger::INFO
     end
 
     def initialize(attributes = {})
@@ -23,16 +45,15 @@ module Anywhere
       @attributes[:stream] ||= STDOUT
     end
 
-    def info(message)
-      print_with_prefix "INFO ".green + " #{message}"
-    end
-
-    def error(message)
-      print_with_prefix "ERROR".red + " #{message}"
-    end
-
-    def debug(message)
-      print_with_prefix "DEBUG".blue + " #{message}"
+    LEVELS.each do |name, l|
+      define_method(name.downcase) do |message|
+        return if log_level > l
+        prefix = "%05s" % [name.to_s]
+        if color = COLORS[l]
+          prefix = prefix.color(color)
+        end
+        print_with_prefix "%s %s" % [prefix, message]
+      end
     end
 
     def print_with_prefix(message)
--- spec/lib/anywhere/logger_spec.rb
+++ spec/lib/anywhere/logger_spec.rb
@@ -6,10 +6,43 @@ describe "Anywhere::Logger" do
   it { should_not be_nil }
   it { subject.prefix.should be_nil }
 
+  it "sets the default log level to INFO" do
+    logger.log_level.should eq(Logger::INFO)
+  end
+
   describe "setting the prefix" do
     it "changes the prefix" do
       subject.prefix = "test"
       subject.prefix.should eq("test")
+    end
+  end
+
+  describe "log levels" do
+    let(:stringio) { StringIO.new }
+    let(:log_level) { Logger::DEBUG }
+
+    subject(:lines) do
+      logger.log_level = log_level
+      logger.stub(:stream) { stringio }
+      logger.info "log info"
+      logger.debug "log debug"
+      stringio.rewind
+      stringio.read.split("\n")
+    end
+
+    it { should be_kind_of(Array) }
+    it { subject.count.should eq(2) }
+    it { subject.at(0).should include("INFO") }
+    it { subject.at(1).should include("DEBUG") }
+
+    describe "with log level being info" do
+      let(:log_level) { Logger::INFO }
+
+      it { subject.count.should eq(1) }
+      it { subject.at(0).should include("INFO") }
+    end
+
+    describe "" do
     end
   end
 end
